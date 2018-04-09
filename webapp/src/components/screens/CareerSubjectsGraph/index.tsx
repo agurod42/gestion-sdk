7b3c0be5e06b18a5642ee81b0daba1f377863457
Agustín Rodríguez <agurz@icloud.com>
2018-04-09 19:17:36 -0300

    replaced react-d3-graph with react-vis-force

--- webapp/src/components/screens/CareerSubjectsGraph/index.tsx
+++ webapp/src/components/screens/CareerSubjectsGraph/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { Graph } from 'react-d3-graph';
 import { AutoSizer } from 'react-virtualized';
+import { InteractiveForceGraph, ForceGraphNode, ForceGraphLink } from 'react-vis-force';
 
 import GestionEnmelon from '../../../services/GestionEnmelon';
 
@@ -29,24 +29,22 @@ export default class CareerSubjectsGraph extends React.Component {
             return (
                 <AutoSizer>
                     {({ height, width }) => (
-                        <Graph
-                            id='subjects-graph' // id is mandatory, if no id is defined rd3g will throw an error
-                            data={this.state.data}
-                            config={{
-                                width: width,
-                                height: height,
-                                nodeHighlightBehavior: true,
-                                node: {
-                                    color: 'lightgreen',
-                                    highlightStrokeColor: 'blue',
-                                    labelProperty: 'title',
-                                    size: 120,
-                                },
-                                link: {
-                                    highlightColor: 'lightblue'
-                                },
+                        <InteractiveForceGraph
+                            simulationOptions={{ 
+                                width: width || window.innerWidth, 
+                                height: height || window.innerHeight - 56 
                             }}
-                        />
+                            labelAttr='title'
+                            highlightDependencies={true}
+                            zoom={true}
+                        >
+                            {this.state.data.nodes.map((node: any) => (
+                                <ForceGraphNode key={node.id} node={node} fill='red' />
+                            ))}
+                            {this.state.data.links.map((link: any) => (
+                                <ForceGraphLink key={`${link.source}-${link.target}`} link={link} />
+                            ))}
+                        </InteractiveForceGraph>
                     )}
                 </AutoSizer>
             );
---
+++ webapp/types/react-vis-force.d.ts
@@ -0,0 +1,29 @@
+
+declare module 'react-vis-force' {
+
+    interface InteractiveForceGraphProps {
+        highlightDependencies?: boolean
+        labelAttr?: string
+        simulationOptions?: any
+        zoom?: boolean
+    }
+
+    interface ForceGraphLinkProps {
+        link?: any
+    }
+
+    interface ForceGraphNodeProps {
+        fill?: string
+        node?: any
+    }
+
+    export class InteractiveForceGraph extends React.Component<InteractiveForceGraphProps, any> {
+    }
+
+    export class ForceGraphLink extends React.Component<ForceGraphLinkProps, any> {
+    }
+
+    export class ForceGraphNode extends React.Component<ForceGraphNodeProps, any> {
+    }
+
+}
